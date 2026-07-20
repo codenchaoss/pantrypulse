148d2b278b956b9641d6b88585ee74d03940a081
navadeep <thatiparthynavadeep@gmail.com>
2026-07-20 19:03:12 +0530

    final phase backend

--- src/main/java/com/pantrypulse/config/OpenApiConfig.java
+++ src/main/java/com/pantrypulse/config/OpenApiConfig.java
@@ -1,12 +1,16 @@
 package com.pantrypulse.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,7 +20,12 @@ public class OpenApiConfig {
 
         final String securitySchemeName = "Bearer Authentication";
 
+        Server railwayServer = new Server();
+        railwayServer.setUrl("https://pantrypulse-production-up.up.railway.app");
+        railwayServer.setDescription("Production Server");
+
         return new OpenAPI()
+                .servers(List.of(railwayServer))
                 .info(new Info()
                         .title("PantryPulse API")
                         .version("1.0")
@@ -24,9 +33,9 @@ public class OpenApiConfig {
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
+                        .addSecuritySchemes(
+                                securitySchemeName,
                                 new SecurityScheme()
-                                        .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
