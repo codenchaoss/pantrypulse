7e2731a6160c5ebe1ad0e4662787da7db397a626
navadeep <thatiparthynavadeep@gmail.com>
2026-07-22 12:14:48 +0530

    Set sender address for password reset email

--- src/main/java/com/pantrypulse/authentication/service/EmailService.java
+++ src/main/java/com/pantrypulse/authentication/service/EmailService.java
@@ -5,32 +5,35 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
-
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String from;
-
     public void sendPasswordResetEmail(String email, String resetLink) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        try {
 
-        message.setFrom(from);
-        message.setTo(email);
-        message.setSubject("PantryPulse Password Reset");
+            SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setText(
-                "Hello,\n\n"
-                + "Click the link below to reset your password:\n\n"
-                + resetLink
-                + "\n\nThis link expires in 30 minutes.\n\n"
-                + "PantryPulse Team"
-        );
+            message.setFrom("yourgmail@gmail.com");
 
-        mailSender.send(message);
+            message.setTo(email);
+
+            message.setSubject("PantryPulse Password Reset");
+
+            message.setText(resetLink);
+
+            mailSender.send(message);
+
+            System.out.println("MAIL SENT SUCCESSFULLY");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            throw e;
+        }
     }
 }
